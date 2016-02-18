650289292e3cffff41e6901663f3f0149d78de2f
jaseg <code@jaseg.net>
2016-02-18 15:06:00 +0100

    Highlighter more or less working now

--- newsgrep.py
+++ newsgrep.py
@@ -2,88 +2,155 @@
 
 import re
 import sys
+import itertools
 
 import grequests
 import humanize
 import bs4
 
 FEEDS = [
-		'http://rss.sueddeutsche.de/app/service/rss/alles/index.rss?output=rss',
-		'http://www.tagesschau.de/xml/rss2',
-		'http://www.spiegel.de/schlagzeilen/index.rss',
-		'http://newsfeed.zeit.de/all',
-		'http://www.faz.net/rss/aktuell',
-		'http://www.taz.de/!p4608;rss/',
-		'http://www.welt.de/?service=Rss',
-		'http://www.handelsblatt.com/contentexport/feed/schlagzeilen']
+        'http://rss.sueddeutsche.de/app/service/rss/alles/index.rss?output=rss',
+        'http://www.tagesschau.de/xml/rss2',
+        'http://www.spiegel.de/schlagzeilen/index.rss',
+        'http://newsfeed.zeit.de/all',
+        'http://www.faz.net/rss/aktuell',
+        'http://www.taz.de/!p4608;rss/',
+        'http://www.welt.de/?service=Rss',
+        'http://www.handelsblatt.com/contentexport/feed/schlagzeilen']
 
 def cut_and_cull(text, leftmost=0, rightmost=0, tolerance=3):
-	words = [w for w in text.split() if not 'http://' in w or '.html' in w] # a bit savage, but works fair enough.
-	if len(words) < leftmost + rightmost + tolerance:
-		return ' '.join(words)
-	return ' '.join(words[:leftmost] + ['[...]'] + words[-rightmost:])
+    words = [w for w in text.split() if not 'http://' in w or '.html' in w] # a bit savage, but works fair enough.
+    if len(words) < leftmost + rightmost + tolerance:
+        return ' '.join(words)
+    return ' '.join(words[:leftmost] + ['[...]'] + words[-rightmost:])
 
-def highlight_match(searchre, text):
-	highlight = lambda s: '\033[93m'+s+'\033[0m'
-	for par in text.split('\n\n'):
-		par = par.strip()
-		positions = [ m.span() for m in searchre.finditer(par) ]
-		if positions:
-			(first, _1), (lastm, last) = positions[0], positions[-1]
-			yield cut_and_cull(par[:first], rightmost=5)
-			yield '\033[91m<1>\033[0m'
-			if len(positions)>1: # handle intervals between matches
-				for (matchs,left),(right,_2) in zip(positions[:-1], positions[1:]):
-					yield '\033[91m<2>\033[0m'
-					yield highlight(par[matchs:left])
-					yield '\033[91m<3>\033[0m'
-					yield cut_and_cull(par[left:right], leftmost=5, rightmost=5)
-			yield '\033[91m<4>\033[0m'
-			yield highlight(par[lastm:last])
-			yield '\033[91m<5>\033[0m'
-			yield cut_and_cull(par[last:], leftmost=5)
-			yield '\033[91m<6>\033[0m'
+def highlight_match(searchre, text, hlext=9, tol=5, sep=['[...]']):
+    text = searchre.sub('\033[93m\\g<0>\033[0m', text)
+
+    print('### INPUT ###')
+    print(text)
+    print('### ===== ###')
+    sentences = text.split('.')
+    hlstate = False
+    hlstack = [[]]
+    for i, st in enumerate(sentences):
+        words = [':START:', *st.split(), ':END:']
+        if len(words) < 3: # empty sentence
+            continue
+        words[-2] += '.' # fix up full stop at end of sentence
+        wkeep = [False] * len(words)
+
+        for j, w in enumerate(words):
+            if '\033[93m' in w:
+                hlstate = True
+            wkeep[j] = hlstate
+            if '\033[0m' in w:
+                hlstate = False
+        wkeep[-1] = False # exclude :END:
+        
+        groups = [ (key, len(list(group))) for key, group in itertools.groupby(wkeep) ]
+        print('groups before:', groups)
+        if len(groups) >= 3: # Single highlight in sentence, possibly entire sequence.
+            (frontkeep, frontlen), *mid, (backkeep, backlen) = groups
+            
+            # reverse iterator to not clobber indices for later iterations
+            for i, (keep, klen) in reversed(list(enumerate(mid, start=1))):
+                if not keep: # no-keep area bounded by keep-areas on both sides
+                    if klen < 2*hlext+tol:
+                        print('CASE 1 ', end='')
+                        groups[i][0] = True
+                    else: 
+                        print('CASE 2 ', end='')
+                        groups = [*groups[:i], (True, hlext), (False, klen-2*hlext), (True, hlext), *groups[i+1:]]
+
+
+            if frontlen < hlext+tol:
+                print('CASE 3 ', end='')
+                groups[0] = (True, frontlen)
+            else:
+                print('CASE 4 ', end='')
+                groups = [(False, frontlen-hlext), (True, hlext), *groups[1:]]
+
+            if backlen < hlext+tol:
+                print('CASE 5 ', end='')
+                groups[-1] = (True, backlen)
+            else:
+                print('CASE 6 ', end='')
+                groups = [*groups[:-1], (True, hlext), (False, backlen-hlext)]
+        print('groups after:', groups)
+        wkeep = [ keep for keeps in ( [keep]*klen for keep, klen in groups ) for keep in keeps ]
+        words, wkeep = words[1:-1], wkeep[1:-1] # cut end delims
+        print('resulting wkeep:', wkeep)
+        newgroups = [ (key, len(list(group))) for key, group in itertools.groupby(wkeep) ]
+        print('newgroups:', newgroups)
+
+        for keep, word in zip(wkeep, words):
+            if keep:
+                print('\033[92m{}\033[0m '.format(word), end='')
+            else:
+                print('\033[91m{}\033[0m '.format(word), end='')
+        print()
+        (frontkeep, frontlen), *rest = newgroups
+
+        if frontkeep: # if necessary, join sentence groups
+            if hlstack[-1] is not sep:
+                hlstack[-1] += words[:frontlen]
+            else:
+                hlstack.append(words[:frontlen])
+        elif hlstack[-1] is not sep:
+            hlstack.append(sep)
+        windex = frontlen
+        for keep, klen in rest:
+            if keep:
+                hlstack.append(words[windex:windex+klen])
+            else:
+                hlstack.append(sep)
+            windex += klen
+    if not hlstack[0]:
+        hlstack = hlstack[1:]
+    return ' '.join( ' '.join(s) for s in hlstack )
 
 searchre = re.compile('|'.join(sys.argv[1:]), flags=re.IGNORECASE)
 nothing_found = []
 lookup_failed = []
 def fail_handler(req, exception):
-	lookup_failed.append(req.url.split('.')[1]) # see below.
+    lookup_failed.append(req.url.split('.')[1]) # see below.
 
 for res in grequests.map((grequests.get(url) for url in FEEDS), exception_handler=fail_handler):
 
-	if not res:
-		continue
+    if not res:
+        continue
 
-	soup = bs4.BeautifulSoup(res.text, 'lxml')
-	found = soup(text=searchre)
-	sitename = res.url.split('.')[1] # somewhat primitive, but works. more useful than the actual feed <title>s
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    found = soup(text=searchre)
+    sitename = res.url.split('.')[1] # somewhat primitive, but works. more useful than the actual feed <title>s
 
-	if not found:
-		nothing_found.append(sitename)
-		continue
+    if not found:
+        nothing_found.append(sitename)
+        continue
 
-	print('Site: \033[96m{}\033[0m'.format(sitename))
-	found_dict = {}
-	for element in found:
-		item = element.find_parent('item')
-		found_dict[item.find('guid').text] = item
-	
-	for link, item in found_dict.items():
-		ce, desc = item.find('content:encoded'), item.find('description')
-		title, date = item.find('title').text, item.find('pubdate').text
-		text = str(ce.text) if ce else str(desc.text)
-		print('In:   \033[92m{}\033[0m'.format(title))
-		print('Link: \033[92m{}\033[0m'.format(link))
-		print('Date: \033[92m{}\033[0m'.format(humanize.naturaltime(date)))
-		for m in highlight_match(searchre, text):
-			print(m, end='')
-		print()
-	print()
+    print('Site: \033[96m{}\033[0m'.format(sitename))
+    found_dict = {}
+    for element in found:
+        item = element.find_parent('item')
+        found_dict[item.find('guid').text] = item
+    
+    for link, item in found_dict.items():
+        ce, desc = item.find('content:encoded'), item.find('description')
+        title, date = item.find('title').text, item.find('pubdate').text
+        text = str(ce.text) if ce else str(desc.text)
+        print('In:   \033[92m{}\033[0m'.format(title))
+        print('Link: \033[92m{}\033[0m'.format(link))
+        print('Date: \033[92m{}\033[0m'.format(humanize.naturaltime(date)))
+        print('Item:')
+        print(item)
+        print(highlight_match(searchre, text))
+        print()
+    print()
 
 if nothing_found:
-	print('Nothing found in:\033[96m', *nothing_found, '\033[0m')
+    print('Nothing found in:\033[96m', *nothing_found, '\033[0m')
 
 if lookup_failed:
-	print('Lookup failed for:\033[91m', *lookup_failed, '\033[0m', file=sys.stderr)
+    print('Lookup failed for:\033[91m', *lookup_failed, '\033[0m', file=sys.stderr)
 
